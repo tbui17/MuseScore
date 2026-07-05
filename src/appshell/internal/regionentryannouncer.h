2513d899ec4cd97d568b9d7875673d08d8cb5123
tbui17 <tbui17@users.noreply.github.com>
2026-07-04 18:30:30 -0600

    feat(appshell): add RegionEntryAnnouncer header

---
+++ src/appshell/internal/regionentryannouncer.h
@@ -0,0 +1,48 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-only
+ * MuseScore-Studio-CLA-applies
+ *
+ * MuseScore Studio
+ * Music Composition & Notation
+ *
+ * Copyright (C) 2021 MuseScore Limited and others
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+#ifndef MU_APPSHELL_REGIONENTRYANNOUNCER_H
+#define MU_APPSHELL_REGIONENTRYANNOUNCER_H
+
+#include "modularity/ioc.h"
+#include "async/asyncable.h"
+#include "accessibility/iaccessibilitycontroller.h"
+#include "context/iuicontextresolver.h"
+
+namespace mu::appshell {
+class RegionEntryAnnouncer : public muse::Contextable, public muse::async::Asyncable
+{
+    muse::ContextInject<context::IUiContextResolver> uiContextResolver = { this };
+    muse::ContextInject<muse::accessibility::IAccessibilityController> accessibilityController = { this };
+
+public:
+    RegionEntryAnnouncer(const muse::modularity::ContextPtr& iocCtx)
+        : muse::Contextable(iocCtx) {}
+
+    void init();
+
+private:
+    void onContextChanged();
+    QString messageForContext(const muse::ui::UiContext& ctx) const;
+};
+}
+
+#endif // MU_APPSHELL_REGIONENTRYANNOUNCER_H
